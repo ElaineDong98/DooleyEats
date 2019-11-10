4067b92430933915285261641356d725992890ea
ElaineDong98 <32375876+ElaineDong98@users.noreply.github.com>
2019-11-10 15:05:03 -0500

    user can change profile image

--- models/User.js
+++ models/User.js
@@ -59,8 +59,9 @@ const UserSchema = new mongoose.Schema({
   },
 
   image: {
-    id: String,
-    url: String
+    type: String,
+    required: false, 
+    default: "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
   }
 
 });
