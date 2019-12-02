d21963e6567e2c5b072fd4a42090447513f5a6b8
yayuzhu <38043497+yayuzhu@users.noreply.github.com>
2019-12-02 11:08:53 -0500

    reference link in dashboard

--- routes/posts.js
+++ routes/posts.js
@@ -49,7 +49,7 @@ router.get("/", (req, res) => {
     let numPost;
     const searchQuery = req.query.search,
           regex = new RegExp(escapeRegex(req.query.search), 'gi');
-    Post.find({"$or": [{tags: regex},{title:regex}]},(err, allPosts) => {
+    Post.find({"$or": [{tags: regex},{title:regex},{postyType:regex}]},(err, allPosts) => {
       if (err) {
         req.flash("error", "Error: Cannot show posts");
         console.log(err);
